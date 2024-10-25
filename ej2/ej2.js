307d572c5499f0ce91064e3ae9a1f15beaffbd97
David <1918862@alu.murciaeduca.es>
2024-10-25 13:39:18 +0200

    25

---
+++ ej2/ej2.js
@@ -0,0 +1,84 @@
+﻿// UT2 cookies
+
+window.onload = function() { 
+
+
+	let canvas;
+	
+	let ctx, x, x2, y, y2;
+	let primero, segundo;
+	
+		function generaDatos() {
+	
+		// Y a su contexto 2d
+		ctx = canvas.getContext('2d');
+	
+		primero = false;
+		segundo = false;
+		
+		// Generamos las coordenadas iniciales
+		x = Math.random()*575;
+		x2 = Math.random()*575;
+		y = 0;
+		y2 = 0;
+		vel1 = 1 + Math.random();
+		vel2 = 1 + Math.random();
+		}
+	
+		function generaAnimación() {
+			
+			  // aproximadamente a 24 frames por segundo (fps)
+			  // Limpiamos el canvas, eliminando el contenido
+			  // desde el punto (0, 0) al punto (600, 400)
+			  ctx.clearRect(0, 0, 600, 400);
+	
+			  // Generamos nuevas coordenadas
+			  // Que basicamente representan un desplazamiento lineal
+				// Y dibujamos nuestra figura
+			  ctx.fillStyle = "#FF0000";
+			  ctx.fillRect(x, y, 25, 25);
+			  
+			  ctx.fillStyle = "#000000";
+			  ctx.fillRect(x2, y2, 25, 25);
+			   
+			   if (y>=375) {
+				   
+				   primero = true;
+			   }
+			   else y=y+vel1 
+			   ;
+	
+			   if (y2>=375) {
+				   
+				   segundo = true;
+			   }
+			   
+			   else y2=y2+vel2;
+	
+			   if ((primero) && (segundo)) {
+	
+				   clearInterval(id);
+			   
+			   }
+	
+		}
+	
+		// Obtenemos una referencia al canvas
+		canvas = document.getElementById('miCanvas');
+	
+		// Y a su contexto 2d
+		ctx = canvas.getContext('2d');
+	
+		generaDatos();
+		
+		let id = setInterval(generaAnimación, 1000/30);
+	
+		generaAnimación();
+	
+	
+	
+	
+	}
+	
+	
+	
--- ejMovimiento.js
+++ ejMovimiento.js
@@ -5,6 +5,22 @@ window.onload = function () {
   let ctx;//Su significado es contexto
   let id;
   let direcccion = false;
+
+function crearcolor(){
+
+const caracters = 'ABCDEF0123456789'
+
+let color =' ';
+for (let i = 0; i < 6; i++) {
+    let posicionAleatoria = Math.floor(Math.random()*caracters.length)
+    color+= caracters[posicionAleatoria];
+    
+}
+cadenaColor = "#"+color;
+return cadenaColor;
+
+}
+
   function pintaRectangulo() {
     ctx.clearRect(0, 0, 500, 500);
     ctx.fillStyle = "#FF0000";
@@ -13,15 +29,25 @@ window.onload = function () {
     if (direcccion === false) {
       x++;
       y++;
+      
+
       if (x === 485) {
         direcccion = true;
+        ctx.fillStyle = crearcolor();
+
       }
     } else if (direcccion === true) {
       x--;
       y--;
       if (x === 0) {
         direcccion = false;
+ HEAD
         ctx.fillStyle()
+
+        ctx.fillStyle = crearcolor();
+
+        
+ 25109e8 (25)
       }
     }
   }
