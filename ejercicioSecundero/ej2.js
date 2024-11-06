f2e88e5537d8651c52e44e646b63d9dcd1de9e4a
David <1918862@alu.murciaeduca.es>
2024-11-06 12:10:52 +0100

    6

---
+++ ejercicioSecundero/ej2.js
@@ -0,0 +1,46 @@
+﻿// UT2 cookies
+
+window.onload = function() { 
+	let canvas, ctx;
+	let secundero =1;
+
+
+		function generaAnimación() {
+			
+			
+			  ctx.clearRect(0, 0, 600, 400);
+	
+			 
+			  ctx.font = "30px Arial";
+			  ctx.fillStyle = "black";
+
+			  ctx.fillText(secundero,  Math.random()*575,Math.random()*375);
+
+			  secundero ++;
+			  
+				if (secundero === 60) {
+					secundero =1;
+					
+				
+				}
+			   
+			  
+	
+		}
+	
+		// Obtenemos una referencia al canvas
+		canvas = document.getElementById('miCanvas');
+	
+		// Y a su contexto 2d
+		ctx = canvas.getContext('2d');
+
+	setInterval(generaAnimación, 1000);
+	
+	
+	
+	
+	
+	}
+	
+	
+	
