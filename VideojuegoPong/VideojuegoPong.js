2c6bb94704fdbeb859419f0801951f6be6690046
David <1918862@alu.murciaeduca.es>
2024-11-08 11:48:24 +0100

    08/Nov

---
+++ VideojuegoPong/VideojuegoPong.js
@@ -0,0 +1,30 @@
+﻿// UT2 cookies
+
+window.onload = function() { 
+	let canvas, ctx;
+	let secundero =1;
+
+
+		function generaAnimación() {
+			
+			
+			
+	
+		}
+	
+		// Obtenemos una referencia al canvas
+		canvas = document.getElementById('miCanvas');
+	
+		// Y a su contexto 2d
+		ctx = canvas.getContext('2d');
+
+	setInterval(generaAnimación, 1000);
+	
+	
+	
+	
+	
+	}
+	
+	
+	
